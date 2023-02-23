dedd995b752d41430d490b1139f1e7b880dd7ea9
Gabrielle Nicole Suharjono <gabrielle.nicole2003@gmail.com>
2023-02-23 16:20:35 +0800

    [App] Added test page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import Home from './pages/Home'
+import Test from './pages/Test'
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Test2 from './pages/Test2'
 
 function App() {
-  return <Home />
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/test' element={<Test />} />
+        <Route path='/test2' element={<Test2 />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
