b2ac386e89972a44732bcd0e95a3844d398c9281
Gabrielle Nicole Suharjono <gabrielle.nicole2003@gmail.com>
2023-03-09 06:30:39 +0800

    Add filter variable to Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,20 @@ import Sidebar from '../components/Sidebar'
 import ShopItem from '../components/ShopItem'
 import { Link } from 'react-router-dom'
 import Axios from 'axios'
+import { useFilter } from '../hooks/useFilter'
 
 export default function Home() {
   const [data, setData] = useState([])
+  const { currentFilter } = useFilter()
   const getItems = () => {
     Axios.get('http://localhost:3000/items').then((response) => {
       setData(response.data)
     })
   }
+
+  console.log(currentFilter.priceRange.join('+'))
+  console.log(currentFilter.sort)
+  console.log(currentFilter.category.join('+'))
 
   useEffect(() => {
     getItems()
