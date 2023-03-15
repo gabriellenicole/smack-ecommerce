602b7e258e0cdc0a7dded65f373fd4976e83ba53
Gabrielle Nicole Suharjono <gabrielle.nicole2003@gmail.com>
2023-03-16 00:21:02 +0800

    Added payment page functionality

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,19 +1,41 @@
 import React from 'react'
 import Cart from '../components/Cart'
+import { useCart } from '../hooks/useCart'
+import { smackAxios } from '../api'
+import { useUser } from '../hooks/useUser'
 
 export default function Login() {
+  const { currentCart, updateCart } = useCart
+  const { currentUser } = useUser
+
+  const deleteApiCall = async (listingId) => {
+    const response = await smackAxios.delete(
+      `api/cart?listing_id=${listingId}&user_id=${currentUser.userId}`
+    )
+    updateCart(response.data)
+  }
+
+  const handlePayment = () => {
+    currentCart?.map((item) => {
+      deleteApiCall(item.listing_id)
+    })
+  }
+
   return (
     <div className='h-[770px] w-screen items-center flex justify-center gap-x-16 bg-beige'>
-      <div className='bg-white p-10 rounded-lg shadow-md'>
+      <div className='bg-white p-10 rounded-lg shadow-md w-2/5 h-[600px]'>
         <Cart />
       </div>
       <div className='bg-white flex flex-col items-center justify-center gap-y-4 shadow-md p-9 rounded-lg py-20'>
         <p className='font-medium text-xl'>Scan to Pay</p>
         <img
           className='w-80'
-          src='https://www.freepnglogos.com/uploads/qr-code-png/qr-code-file-bangla-mobile-code-0.png'
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png'
         ></img>
-        <button className='button-class bg-orange p-3 rounded-lg text-white font-medium'>
+        <button
+          onClick={handlePayment}
+          className='button-class bg-orange p-3 rounded-lg text-white font-medium'
+        >
           Payment Finished
         </button>
       </div>
